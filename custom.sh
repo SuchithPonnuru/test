bf4f7a3866aacf805e4eae6201a1ec71bc65ac78
SuchithPonnuru <89370746+SuchithPonnuru@users.noreply.github.com>
2021-11-02 15:17:25 +0530

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$1 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+
+
+sudo chmod -R 755 /etc/datadog-agent/datadog.yaml
+
+
+sudo sed -i 's/# logs_enabled: false/logs_enabled: true/g' /etc/datadog-agent/datadog.yaml
+
+sudo systemctl restart datadog-agent
+
